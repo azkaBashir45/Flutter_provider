3c54f7b6fe2952a5d7a8d8b05ff18508ffe537f4
azkaBashir45 <azka.ngxoft@gmail.com>
2021-08-31 12:54:22 -0700

    logic write

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: TodoApp());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: TodoApp(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -22,6 +26,25 @@ class TodoApp extends StatefulWidget {
 class _TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+    );
   }
+}
+
+class Task {
+  String? title;
+  bool? isDone;
+  Task({this.title, this.isDone});
+
+}
+class TaskData{
+ 
+ List<Task> listData=[
+   Task(title:'Book', isDone:false)
+ ];
+
 }
