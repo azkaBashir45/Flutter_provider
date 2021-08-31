89eb1d9f6e2548d69a829846ed2931ab8a61e46e
azkaBashir45 <azka.ngxoft@gmail.com>
2021-08-31 12:44:26 -0700

    main simple code

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: TodoApp());
+  }
+}
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  _TodoAppState createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
